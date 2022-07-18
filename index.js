c4b74416568b88f608bebd1ac108ac398e195c5c
AyusshVa <ayusshvashishth@gmail.com>
2022-07-18 10:42:47 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const multer = require("multer");
 dotenv.config();
 
 const mongoose = require("mongoose");
@@ -15,12 +16,32 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("connected to the mongo server");
-    app.use("/api/auth", authRoute); // request of /api/ is sent to the register Route.
-    app.use("/api/user", userRoute); // request of /api/ is sent to the register Route.
-    app.use("/api/post", postRoute); // request of /api/ is sent to the register Route.
-    app.use("/api/category", CategoryRoute); // request of /api/ is sent to the register Route.
   })
   .catch((err) => console.log(err.message));
+
+// using multer for storing files.
+
+const storage = multer.diskStorage({
+  // this function is like storing the files.
+  destination: (req, file, cb) => {
+    //decides where will the file go.
+    // it will take 3 params, req, file(the uploaded file), and a callback to handle err
+    cb(null, "./images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage }); // function to upload files.
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file has uploaded");
+});
+
+app.use("/api/auth", authRoute); // request of /api/ is sent to the register Route.
+app.use("/api/user", userRoute); // request of /api/ is sent to the register Route.
+app.use("/api/post", postRoute); // request of /api/ is sent to the register Route.
+app.use("/api/category", CategoryRoute); // request of /api/ is sent to the register Route.
 
 app.listen("5000", () => {
   console.log("server is running and up at port 5000");
